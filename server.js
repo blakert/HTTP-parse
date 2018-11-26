c4609910f40b19c01fe01a0968fcc2bbcd8f8a5d
Glitch (hello-express) <none>
2018-11-26 00:31:44 +0000

    😘🌛 Checkpoint ./server.js:2378677/878

--- server.js
+++ server.js
@@ -1,21 +1,31 @@
 // server.js
-// where your node app starts
+
 
 // init project
 const express = require('express');
 const app = express();
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
+
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
+app.get('/api/whoami',function(req,res){
+  //get ip address from req header
+  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  const language = req.headers["accept-language"];
+  const sysInfo = req.headers["user-agent"];
+  
+  const responseObj = {
+    "ip adrress": ip,
+    "language": language,
+    "system info": sysInfo
+  }
+  res.send(responseObj);
+});
 // listen for requests :)
 const listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
